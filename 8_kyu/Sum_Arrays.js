3e5399a65ff52fade99faadb5e42ecb9c5ac5886
Vladimir <manaenko1@gmail.com>
2022-11-15 22:08:40 +0200

    Sum Arrays

---
+++ 8_kyu/Sum_Arrays.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/53dc54212259ed3d4f00071c
+
+function sum (numbers) {
+  "use strict";
+  return numbers.reduce((total, currentValue) => total + currentValue, 0); 
+};
+
