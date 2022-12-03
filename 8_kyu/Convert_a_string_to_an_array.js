e5a7cbfaf25d7c0500b6d75f0d8140936dbdcf8b
Vladimir <manaenko1@gmail.com>
2022-12-03 09:29:50 +0200

    Convert a string to an array

---
+++ 8_kyu/Convert_a_string_to_an_array.js
@@ -0,0 +1,12 @@
+// https://www.codewars.com/kata/57e76bc428d6fbc2d500036d
+
+// variant one
+function stringToArray(string) {
+
+  return string.split(' ');
+
+}
+
+// variant two
+let stringToArray = (string) => string.split(' ');
+
