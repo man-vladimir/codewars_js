16e9612477d0931562a9db77c3ba75054563647a
Vladimir <manaenko1@gmail.com>
2022-12-04 11:21:58 +0200

    Refactor 2 solution

--- 8_kyu/Square_n__Sum.js
+++ 8_kyu/Square_n__Sum.js
@@ -1,5 +1,4 @@
 // https://www.codewars.com/kata/515e271a311df0350d00000f
 
-let initialValue = 0;
-let squareSum = numbers => numbers.reduce((sum, value) => sum + value ** 2, initialValue)
+let squareSum = numbers => numbers.reduce((sum, value) => sum + value ** 2, 0)
 
