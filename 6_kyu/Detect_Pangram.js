ebd4781832d2ec11c9353b1867aae1e9a0de14a4
Vladimir <manaenko1@gmail.com>
2022-12-06 13:54:07 +0200

    Detect Pangram

---
+++ 6_kyu/Detect_Pangram.js
@@ -0,0 +1,26 @@
+// https://www.codewars.com/kata/545cedaa9943f7fe7b000048
+
+// variant one
+let isPangram = string => {
+  const alphabet = "abcdefghijklmnopqrstuvwxyz";
+  const regex = /\s|[\.\?\,\:\;\!]|\d/g;
+  const lowercase = string.toLowerCase().replace(regex, "");
+  
+  for(let i = 0; i < alphabet.length; i ++){
+    if(lowercase.indexOf(alphabet[i]) === -1) return false
+  }
+  return true
+}
+
+// variant two
+let isPangram = string => {
+  const alphabet = "abcdefghijklmnopqrstuvwxyz";
+  const regex = /\s|[\,\.]|\d/g;
+  const lowercase = string.toLowerCase().replace(regex, "");
+  const uniqString = new Set(lowercase)  
+  
+  if(uniqString.size != alphabet.length) return false  
+  
+  return true
+}
+
