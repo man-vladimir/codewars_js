423983f5345b7e94360722c0488965b22a153824
Vladimir <manaenko1@gmail.com>
2023-01-17 20:15:01 +0200

    Refactor solution

--- 8_kyu/Set_Alarm.js
+++ 8_kyu/Set_Alarm.js
@@ -1,6 +1,5 @@
 // https://www.codewars.com/kata/568dcc3c7f12767a62000038
 
-let setAlarm = (employed, vacation) => {
-  return employed && !vacation
-}
+let setAlarm = (employed, vacation) => employed && !vacation;
+
 
