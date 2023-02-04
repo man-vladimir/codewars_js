90b0f6a2f221db0beb30e41ead239fe99c74c176
Vladimir <manaenko1@gmail.com>
2023-02-04 12:17:47 +0200

    Who likes it?

---
+++ 6_kyu/Who_likes_it.js
@@ -0,0 +1,12 @@
+// https://www.codewars.com/kata/5266876b8f4bf2da9b000362
+
+let likes = names => {
+  switch (names.length){
+      case 0 : return "no one likes this"; break
+      case 1 : return `${names[0]} likes this`; break
+      case 2 : return `${names.join(' and ')} like this`; break
+      case 3 : return `${names[0]}, ${names[1]} and ${names[2]} like this`; break
+      default: return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`
+  }    
+}
+
