753fcfae6b92e118d8e9c39e96d54394ef608c74
Vladimir <manaenko1@gmail.com>
2023-02-09 12:08:27 +0200

    Grasshopper - Check for factor

---
+++ 8_kyu/Grasshopper-Check_for_factor.js
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/55cbc3586671f6aa070000fb
+
+// varian one
+let checkForFactor = (base, factor) => base % factor === 0;
+
+// variant two
+let checkForFactor2 = (base, factor) => !(base % factor);
+
