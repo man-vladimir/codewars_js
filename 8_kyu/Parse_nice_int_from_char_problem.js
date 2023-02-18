b0378bbf7502276ef8c33b68be92ed535ed2ff8e
Vladimir <manaenko1@gmail.com>
2023-02-18 10:40:44 +0200

    Parse nice int from char problem

---
+++ 8_kyu/Parse_nice_int_from_char_problem.js
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/557cd6882bfa3c8a9f0000c1
+
+// variant one
+const getAge = inputString => parseInt(inputString, 10);
+
+// variant two
+const getAge2 = inputString => +inputString[0];
+
