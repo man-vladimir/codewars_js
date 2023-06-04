40ae3c75e090b9e353d30f732fec3e1f31cb2efa
Vladimir <manaenko1@gmail.com>
2023-06-04 11:32:05 +0300

    Descending Order

---
+++ 7_kyu/Descending_Order.js
@@ -0,0 +1,7 @@
+//https://www.codewars.com/kata/5467e4d82edf8bbf40000155
+
+const descendingOrder = n => {
+    let array = (""+n).split("").map(Number);
+    return Number(array.sort((a,b) => b - a).join(''));
+}
+
